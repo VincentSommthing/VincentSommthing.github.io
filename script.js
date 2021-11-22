7c464d6c86cacb6a983300e1d3a5e78c505520a6
Vincent Sommthing <46465585+VincentSommthing@users.noreply.github.com>
2021-11-21 18:51:18 -0800

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var xhttp = new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
        // Typical action to be performed when the document is ready:
-       //document.getElementById("demo").innerHTML = xhttp.responseText;
+       document.body.innerHTML = xhttp.responseText;
     }
 };
 xhttp.open("GET", "images.json", true);
