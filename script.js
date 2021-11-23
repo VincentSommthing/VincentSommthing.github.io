1386f36c2bedb4f960c2ca078d05e7322a0b30f4
Vincent Sommthing <46465585+VincentSommthing@users.noreply.github.com>
2021-11-22 16:42:12 -0800

    Add files via upload
    added zooming up to images
    theres a bug where if you're scrolling while a transition is happening, the final state does not update to follow the scrolling.

--- script.js
+++ script.js
@@ -27,13 +27,78 @@ function setUpImages(stuff) {
 function createImage(item) {
 
 	const newImage = document.createElement("img");
-	newImage.className += "equalSpacing";
+	newImage.className += "listImg";
 	newImage.setAttribute("src", item.url);
 	newImage.style.width = "100%";
+	newImage.onclick = onImgClick;
 
 	const imgContainer = document.createElement("div");
 	imgContainer.className += "imgContainer";
 	imgContainer.appendChild(newImage);
 
 	imgListEl.appendChild(imgContainer);
+}
+
+const megaImg = document.getElementById("megaImg");
+const megaContainer = document.getElementById("megaImgContainer");
+const zoomBg = document.getElementById("zoomBG");
+
+megaContainer.onclick = leaveZoom;
+megaImg.onclick = leaveZoom;
+var selectedImgRect;
+var selectedImg;
+var selectedImgContainer;
+//https://stackoverflow.com/questions/21686827/css-transform-a-div-without-affecting-surrounding-content
+//https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Transitions/Using_CSS_transitions
+function onImgClick(e) {
+	selectedImg = this;
+	selectedImgContainer = this.parentNode;
+
+	//set the zoom image container to be where the original image is
+	let rect = selectedImgContainer.getBoundingClientRect();
+	megaContainer.style.left = rect.x + "px";
+	megaContainer.style.top = rect.y + "px";
+	megaContainer.style.width = rect.width + "px";
+	megaContainer.style.height = rect.height + "px";
+	//https://stackoverflow.com/questions/32481972/transition-not-working-when-changing-from-display-none-to-block
+	zoomBg.style.display = "block";
+	megaContainer.style.display = "flex";
+
+	setTimeout(function() {
+		zoomBg.classList.add("zoomActive");
+		megaContainer.classList.add("zoomActive");
+		megaImg.classList.add("zoomActive");
+
+		//zoom up the image container to fill the whole screen
+		megaContainer.style.left = "0px";
+		megaContainer.style.top = "0px";
+		megaContainer.style.width = "100vw";
+		megaContainer.style.height = "100vh";
+	}, 0);
+
+	//https://stackoverflow.com/questions/442404/retrieve-the-position-x-y-of-an-html-element
+
+	this.classList.add("zoomActive");
+	megaImg.setAttribute("src", this.getAttribute("src"));
+}
+
+function leaveZoom(e) {
+	zoomBg.classList.remove("zoomActive");
+	megaContainer.classList.remove("zoomActive");
+	megaImg.classList.remove("zoomActive");
+	megaImg.classList.add("zoomOut");
+
+	//set position and dimensions back to original image
+	let rect = selectedImgContainer.getBoundingClientRect();
+	megaContainer.style.left = rect.x + "px";
+	megaContainer.style.top = rect.y + "px";
+	megaContainer.style.width = rect.width + "px";
+	megaContainer.style.height = rect.height + "px";
+
+	setTimeout(function() {
+		zoomBg.style.display = "none";
+		megaContainer.style.display = "none";
+		selectedImg.classList.remove("zoomActive");
+		megaImg.classList.remove("zoomOut");
+	}, 500);
 }
